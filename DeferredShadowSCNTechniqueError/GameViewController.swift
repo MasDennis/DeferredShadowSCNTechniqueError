d0d2df218a6414b751dfd3de51c5732df86ffa34
MasDennis <ippeldv@gmail.com>
2019-01-30 15:55:51 +0100

    Initial commit.

---
+++ DeferredShadowSCNTechniqueError/GameViewController.swift
@@ -0,0 +1,76 @@
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = SCNScene()
+        
+        let sphereGeometry = SCNSphere(radius: 1)
+        let sphereNode = SCNNode(geometry: sphereGeometry)
+        sphereNode.position.y = 1.5
+        sphereNode.categoryBitMask = 2
+        scene.rootNode.addChildNode(sphereNode)
+        
+        let planeGeometry = SCNPlane(width: 4, height: 4)
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.eulerAngles = SCNVector3(-Float.pi / 2, 0, 0)
+        scene.rootNode.addChildNode(planeNode)
+        
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 15)
+        scene.rootNode.addChildNode(cameraNode)
+        
+        let scnView = self.view as! SCNView
+        
+        scnView.scene = scene
+        scnView.allowsCameraControl = true
+        scnView.showsStatistics = true
+        scnView.backgroundColor = UIColor.black
+        
+        let dirLightNode = SCNNode()
+        dirLightNode.light = SCNLight()
+        dirLightNode.light!.type = .directional
+        dirLightNode.light!.castsShadow = true
+        dirLightNode.light!.shadowMode = .deferred
+        dirLightNode.light!.shadowColor = UIColor.black.withAlphaComponent(0.75)
+        dirLightNode.eulerAngles = SCNVector3(-Float.pi / 2, 0, 0)
+        scnView.scene?.rootNode.addChildNode(dirLightNode)
+        
+        createTechnique()
+    }
+    
+    func createTechnique() {
+        let view = self.view as! SCNView
+        
+        if let path = Bundle.main.path(forResource: "BasicTechnique", ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: path)  {
+                let dict2 = dict as! [String : AnyObject]
+                let technique = SCNTechnique(dictionary:dict2)
+                
+                view.technique = technique
+            }
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+}
---
+++ DeferredShadowSCNTechniqueError/Technique/Shaders.metal
@@ -0,0 +1,62 @@
+#include <metal_stdlib>
+using namespace metal;
+#include <SceneKit/scn_metal>
+
+constexpr sampler s = sampler(coord::normalized,
+                              r_address::clamp_to_edge,
+                              t_address::repeat,
+                              filter::linear);
+
+struct CustomNode {
+    float4x4 modelViewProjectionTransform;
+};
+
+struct VertexIn
+{
+    float4 position [[attribute(SCNVertexSemanticPosition)]];
+    float4 normal [[attribute(SCNVertexSemanticNormal)]];
+};
+
+struct VertexOut
+{
+    float4 position [[position]];
+    float2 uv;
+    float4 normal;
+};
+
+vertex VertexOut colour_overlay_vertex(VertexIn in [[stage_in]],
+                                  constant CustomNode& scn_node [[buffer(0)]])
+{
+    VertexOut out;
+    out.position = scn_node.modelViewProjectionTransform * float4(in.position.xyz, 1.0);
+    return out;
+};
+
+fragment half4 colour_overlay_fragment(VertexOut in [[stage_in]],
+                                  texture2d<float, access::sample> colorSampler [[texture(0)]])
+{
+    return half4(1.0);
+};
+
+vertex VertexOut combine_vertex(VertexIn in [[stage_in]],
+                                constant SCNSceneBuffer& scn_frame [[buffer(1)]])
+{
+    VertexOut out;
+    out.position = in.position;
+    out.uv = float2( (in.position.x + 1.0) * 0.5, 1.0 - (in.position.y + 1.0) * 0.5 );
+    return out;
+};
+
+fragment half4 combine_fragment(VertexOut vert [[stage_in]],
+                                texture2d<float, access::sample> colorSampler [[texture(0)]],
+                                texture2d<float, access::sample> overlaySampler [[texture(1)]])
+{
+    float4 fragmentColor = colorSampler.sample(s, vert.uv);
+    float4 overlayColor = overlaySampler.sample(s, vert.uv);
+    
+    if (overlayColor.g < 1.0 ) {
+        return half4(fragmentColor);
+    }
+    
+    return half4(overlayColor);
+}
